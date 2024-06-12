05f1a082f907b7c8f68ecc51e2164a0705a0c943
henrywong1 <henrywong1221@gmail.com>
2024-06-12 15:59:35 -0700

    renamed colorscheme - colorized osaka

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
       import = "lazyvim.plugins",
       opts = {
         colorscheme = "catppuccin",
-        -- colorscheme = "solarized-osaka",
       },
     },
     -- import any extras modules here
