e114145a790eeb75cb50687128c4f7a423e02b5b
henrywong1 <henrywong1221@gmail.com>
2024-06-19 21:50:19 -0700

    added save on file exit. Messed around with other settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -23,3 +23,11 @@ vim.opt.ignorecase = true
 vim.opt.wrap = false
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command("silent update")
+    end
+  end,
+})
