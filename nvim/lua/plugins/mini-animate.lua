eb5de5a8dcb2979be51c4079ec9499fab18dcaf9
henrywong1 <henrywong1221@gmail.com>
2024-12-15 20:05:25 -0800

    Disabled mini.animate scroll issue on ctrl d

--- nvim/lua/plugins/mini-animate.lua
+++ nvim/lua/plugins/mini-animate.lua
@@ -6,6 +6,7 @@ return {
       require("mini.animate").setup({
         open = { enable = false },
         close = { enable = false },
+        scroll = { enabled = false },
       }),
     }
   end,
